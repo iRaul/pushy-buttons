606cd5ff515b6245d87f57eae1670bb7a25d393e
Raul Dronca <rauldronk@gmail.com>
2019-02-21 21:09:38 +0200

    changed the classes name

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,30 @@ var gulp = require('gulp'),
     cssnano = require('gulp-cssnano');
 
 gulp.task('css', function () {
-    return gulp.src('./scss/pushy-buttons.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('./css'))
-    .pipe(cssnano())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({stream:true}));
+  return gulp.src('./scss/pushy-buttons.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer('last 2 version'))
+  .pipe(gulp.dest('./css'))
+  .pipe(cssnano())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('./css'))
+  .pipe(browserSync.reload({stream:true}));
 });
 
 
 gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        server: {
-            baseDir: "./"
-        }
-    });
+  browserSync.init(null, {
+    server: {
+      baseDir: "./"
+    }
+  });
 });
 
 gulp.task('bs-reload', function () {
-    browserSync.reload();
+  browserSync.reload();
 });
 
 gulp.task('default', ['css', 'browser-sync'], function () {
-    gulp.watch("./scss/**/*.scss", ['css']);
-    gulp.watch("./*.html", ['bs-reload']);
+  gulp.watch("./scss/**/*.scss", ['css']);
+  gulp.watch("./*.html", ['bs-reload']);
 });
